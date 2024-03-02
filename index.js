828a16d26af68e765952b40443a7c462379d4bfd
gillsonpassos <oliveirapassos07@outlook.com>
2024-03-02 10:26:48 -0300

    Middlewares & DB com knex #02

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db')
+const consign = require('consign')
 
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
+
+app.db = db
 
 app.listen(4000, () => {
-    console.log('Backend executando')
+    console.log('Backend executando...')
 })
+
